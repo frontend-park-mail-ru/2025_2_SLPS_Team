a4a050b256c30af44034a8aaf4a19d092c47060f
PavlovskiiRomanMikhailovich <romanpavlovsk0808@gmail.com>
2025-09-28 11:03:56 +0300

    fix: button type

--- components/atoms/FormButtons/FormButton.js
+++ components/atoms/FormButtons/FormButton.js
@@ -2,7 +2,7 @@ export async function renderFormButton(container, type, text, classType, onClick
     const response = await fetch('./components/atoms/FormButtons/FormButton.hbs');
     const templateSource = await response.text();
     const template = Handlebars.compile(templateSource);
-    const html = template({classType,text});
+    const html = template({classType,text,type});
 
     const wrapper = document.createElement("div");
     wrapper.innerHTML = html.trim();
