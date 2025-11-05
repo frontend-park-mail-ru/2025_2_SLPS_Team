40e5b6154e5c98546c00785cc3489b3fdad29d41
PavlovskiiRomanMikhailovich <romanpavlovsk0808@gmail.com>
2025-11-05 04:08:33 +0300

    fix dotenv

--- src/services/WebSocketService.js
+++ src/services/WebSocketService.js
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 class WebSocketService {
     static instance = null;
 
@@ -63,4 +67,4 @@ class WebSocketService {
     }
 }
 
-export const wsService = new WebSocketService(process.WS_URL || "ws://localhost:8080/api/ws");
+export const wsService = new WebSocketService(process.env.WS_URL || "ws://localhost:8080/api/ws");
