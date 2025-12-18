0573e7078bc9b2d1bf1e1efe9475bf0c9998fc11
PavlovskiiRomanMikhailovich <romanpavlovsk0808@gmail.com>
2025-12-18 18:00:19 +0300

    fix avatar

--- src/helpers/svgHelper.ts
+++ src/helpers/svgHelper.ts
@@ -1,13 +1,21 @@
-// helpers/svgHelper.ts
-import Handlebars from 'handlebars/runtime'; // <- очень важно!
+import Handlebars from 'handlebars/runtime';
 
 Handlebars.registerHelper('svg', function (src: string, options: any) {
     const className = options.hash.class ?? '';
-    return new Handlebars.SafeString(`
-        <svg class="${className}">
-            <use href="${src}"></use>
-        </svg>
-    `);
+
+    const isSvg = src.trim().toLowerCase().endsWith('.svg');
+
+    if (isSvg) {
+        return new Handlebars.SafeString(`
+            <svg class="${className}">
+                <use href="${src}"></use>
+            </svg>
+        `);
+    } else {
+        return new Handlebars.SafeString(`
+            <img src="${src}" class="${className}" alt="image" />
+        `);
+    }
 });
 
 export {};
